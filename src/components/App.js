e3320f2dc6d8fa14e30fd2aed214f2f275691d3d
Rey David Cuevas <cuevas.vela@gmail.com>
2020-10-02 15:25:24 -0500

    created a basic component Home and added on the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  return <div>Find Your Course APP</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="container mx-auto max-w-screen-xl">
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
