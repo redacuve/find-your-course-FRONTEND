8b32b9c57b7d3622ab6b3e67867ca51614d9039e
Rey David Cuevas <cuevas.vela@gmail.com>
2020-10-12 12:07:23 -0500

    created warning component and added to login and signup functional components

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import Warning from '../components/Warning';
 import {
   UserLogin,
   setLoginLoading,
@@ -21,69 +22,66 @@ function Login(props) {
   };
 
   return (
-    <form
-      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      onSubmit={handleSubmit(handleLogin)}
-    >
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="username"
-        >
-          Email
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          type="email"
-          placeholder="E-mail"
-          name="email"
-          ref={register({ required: true })}
-        />
-        {errors.email && (
-          <span className="text-red-500 text-xs italic">
-            This field is required
-          </span>
-        )}
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          name="password"
-          placeholder="******************"
-          ref={register({ required: true })}
-        />
-        {errors.password && (
-          <span className="text-red-500 text-xs italic">
-            This field is required
-          </span>
-        )}
-      </div>
-      <div className="flex justify-end">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? 'Loading...' : 'Log In'}
-        </button>
-      </div>
-      {error && (
-        <>
-          <small style={{ color: 'red' }}>{error}</small>
-          <br />
-        </>
-      )}
-      <br />
-    </form>
+    <>
+      {error && <Warning error={error} />}
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit(handleLogin)}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            ref={register({ required: true })}
+          />
+          {errors.email && (
+            <span className="text-red-500 text-xs italic">
+              This field is required
+            </span>
+          )}
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            name="password"
+            placeholder="******************"
+            ref={register({ required: true })}
+          />
+          {errors.password && (
+            <span className="text-red-500 text-xs italic">
+              This field is required
+            </span>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? 'Loading...' : 'Log In'}
+          </button>
+        </div>
+        <br />
+      </form>
+    </>
   );
 }
 
