3f8568bf4e3539c98bc761fe46227f492de12bae
Rey David Cuevas <cuevas.vela@gmail.com>
2020-10-13 17:20:06 -0500

    added style in the home component, and added a background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,46 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setTitle } from '../actions/Title';
+import Background from '../assets/img/background.jpg';
 
 function Home() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setTitle('Find Your Courses'));
   }, []);
+  const style = {
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover',
+  };
   return (
-    <div>
-      <h2>Find your Course Anywhere</h2>
-      <p>The Learning platform</p>
+    <div className="home flex items-center" style={style}>
       <div>
-        Start free today
-        Create your free account
-        <Link to="/signup" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Sign in</Link>
-        or
-        <Link to="/login" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</Link>
+        <h2 className="text-center text-3xl font-bold text-white pb-5">
+          Find your Course Anywhere
+        </h2>
+        <p className="font-bold text-white text-center underline">
+          The Learning platform
+        </p>
+        <div>
+          <p className="text-white text-center">
+            Start free today Create your free account
+          </p>
+          <div className="text-center pt-3">
+            <Link
+              to="/signup"
+              className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 border border-orange-700 rounded"
+            >
+              Sign in
+            </Link>
+            <span className="text-white">or</span>
+            <Link
+              to="/login"
+              className="bg-transparent hover:bg-orange-500 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
